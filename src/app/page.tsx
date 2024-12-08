cfde4ea1186ad35cadc16bc1705671fddd8d9e2a
TalhaShaikh150 <tssmart233@gmail.com>
2024-12-09 00:50:32 +0500

    second comment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import Navbar from '../Components/Navbar'
-import Hero from '../Components/Hero'
-import Browse from '../Components/Browse'
-import Products from '../Components/Products'
-import Rooms from '../Components/Rooms'
+import Navbar from '../components/Navbar'
+import Hero from '../components/Hero'
+import Browse from '../components/Browse'
+import Products from '../components/Products'
+import Rooms from '../components/Rooms'
 
 export default function Home() {
   return (
